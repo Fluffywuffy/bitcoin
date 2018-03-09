0f792d7d78d93dd8ba9faeb652f55db276b4febe
Fluffywuffy <timmywelch08@hotmail.com>
2018-03-09 09:34:37 -0500

    add to files

---
+++ javascript/bitcoinOne.js
@@ -0,0 +1,16 @@
+
+    function jr() {
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        document.getElementById("myGraph").style.width = myArr[0] ;
+         document.getElementById("myGraphOne").style.width = myArr[1];
+          document.getElementById("myGraphTwo").style.width = myArr[2];
+          document.getElementById("myGraphThree").style.width = myArr[3];
+    }
+};
+
+xmlhttp.open("GET", "json/bitcoinOne.json", true);
+xmlhttp.send();
+};
