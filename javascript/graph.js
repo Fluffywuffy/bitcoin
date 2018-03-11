bd918f535a92828316dae4e5c0145e16123119da
Fluffywuffy <timmywelch08@hotmail.com>
2018-03-11 19:01:18 -0400

    images and ajax update

--- javascript/graph.js
+++ javascript/graph.js
@@ -1,14 +1,14 @@
-function about() {
+function graph() {
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
-    document.getElementById("pageOne").innerHTML = myArr[0] + myArr[1] + myArr[2] + myArr[3] +
+    document.getElementById("pageOne").innerHTML =myArr[0] + myArr[1] + myArr[2] + myArr[3] +
     myArr[4] + myArr[5];
 
 }
 };
 
-xmlhttp.open("GET", "json/about.json", true);
+xmlhttp.open("GET", "json/graph.json", true);
 xmlhttp.send();
 };
