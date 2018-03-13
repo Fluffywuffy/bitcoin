08896269f9c1271f74236ea33b298db959bb2ff0
Fluffywuffy <timmywelch08@gmail.com>
2018-03-13 08:08:36 -0400

    this is update to the main system

--- javascript/About.js
+++ javascript/About.js
@@ -3,8 +3,12 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
+
     document.getElementById("pageOne").innerHTML = myArr[0] + myArr[1] + myArr[2] + myArr[3] +
     myArr[4] + myArr[5];
+
+    document.getElementById("pageOne").innerHTML = myArr[0] + myArr[1];
+
 
 }
 };
